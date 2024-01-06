81e9fdeec7790d770eabbf5dd326e6caab218335
Iby1234 <reckeriby@gmail.com>
2024-01-06 11:32:50 +0000

    Attempted DB Creator in Python, then began C# Version as it should run faster

---
+++ GenerateDB/Program.cs
@@ -0,0 +1,17 @@
+﻿class CornerCube{
+    public CornerCube(){
+        int[][][] corners = new int[6][][];
+        for (int i = 0; i < corners.Length; i++)
+        {
+            corners[i] = new int[2][];
+            for (int j = 0; j < corners[i].Length; j++)
+            {
+                corners[i][j] = new int[2];
+            }
+        }
+    }
+
+    public void rotateFace(int face, int dir){
+        
+    }
+}
