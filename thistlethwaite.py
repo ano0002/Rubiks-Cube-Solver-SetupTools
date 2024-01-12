a29ce823e630a8c68844819aaee85dcdc0fb7b11
Iby1234 <reckeriby@gmail.com>
2024-01-12 19:21:09 +0000

    Solution fully implemented!
    - Outputs True if cube correctly solved (this has always been the case for 100+ tests)
    - Outputs list of moves (in tuple form)
    - Yet to link to GUI

--- thistlethwaite.py
+++ thistlethwaite.py
@@ -20,6 +20,10 @@ class Thistlethwaite:
         mask, maskTo = self.maskDB.getG1Mask()
         return self.masker.mask(cube, mask, maskTo)
 
+    def G2Mask(self, cube: Cube) -> Cube:
+        mask, maskTo = self.maskDB.getG2Mask()
+        return self.masker.mask(cube, mask, maskTo)
+
     def setMovesG0(self):
         # All moves
         self.validMoves = [(0,1,1), (0,-1,1), (0,1,2), (1,1,1), (1,-1,1), (1,1,2), (2,1,1), (2,-1,1), (2,1,2), (3,1,1), (3,-1,1), (3,1,2), (4,1,1), (4,-1,1), (4,1,2), (5,1,1), (5,-1,1), (5,1,2)]
@@ -28,7 +32,15 @@ class Thistlethwaite:
         #U, F2, R, B2, L, D
         self.validMoves = [(0,1,1), (0,-1,1), (0,1,2), (1,1,2), (2,1,1), (2,-1,1), (2,1,2), (3,1,2), (4,1,1), (4,-1,1), (4,1,2), (5,1,1), (5,-1,1), (5,1,2)]
 
-    def G0(self, cube: Cube) -> Cube:
+    def setMovesG2(self):
+        # U, D, F2, B2, R2, L2
+        self.validMoves = [(0,1,1), (0,-1,1), (0,1,2), (1,1,2), (2,1,2), (3,1,2), (4,1,2), (5,1,1), (5,-1,1), (5,1,2)]
+
+    def setMovesG3(self):
+        # U2, D2, F2, B2, R2, L2
+        self.validMoves = [(0,1,2), (1,1,2), (2,1,2), (3,1,2), (4,1,2), (5,1,2)]
+
+    def G0(self, cube: Cube) -> (Cube, list):
         self.setMovesG0()
         with open(r"Data\G0.json", "r") as f:
             g0Table = json.load(f)
@@ -37,9 +49,9 @@ class Thistlethwaite:
         for move in solution:
             for i in range(move[2]):
                 cube.rotateFace(move[0], move[1])
-        return cube
+        return cube, solution
     
-    def G1(self, cube: Cube) -> Cube:
+    def G1(self, cube: Cube) -> (Cube, list):
         self.setMovesG1()
         with open(r"Data\G1.json", "r") as f:
             g1Table = json.load(f)
@@ -48,7 +60,28 @@ class Thistlethwaite:
         for move in solution:
             for i in range(move[2]):
                 cube.rotateFace(move[0], move[1])
-        return cube
+        return cube, solution
+
+    def G2(self, cube: Cube) -> (Cube, list):
+        self.setMovesG2()
+        with open(r"Data\G2.json", "r") as f:
+            g2Table = json.load(f)
+        maskedCube = self.G2Mask(cube)
+        solution = self.IDDFS(maskedCube, g2Table, 6, 100)
+        for move in solution:
+            for i in range(move[2]):
+                cube.rotateFace(move[0], move[1])
+        return cube, solution
+    
+    def G3(self, cube: Cube) -> (Cube, list):
+        self.setMovesG3()
+        with open(r"Data\G3.json", "r") as f:
+            g3Table = json.load(f)
+        solution = self.IDDFS(cube, g3Table, 10, 100)
+        for move in solution:
+            for i in range(move[2]):
+                cube.rotateFace(move[0], move[1])
+        return cube, solution
 
     def DFS(self, cube: Cube, solution: list, depthRemaining: int, table: dict, tableMaxDepth: int) -> str:
         if self.solver.isSolved(cube):
@@ -83,14 +116,28 @@ class Thistlethwaite:
                 return solution
         else:
             return None
+        
+    def Solve(self, cube: Cube) -> (Cube, list):
+        solution = []
+        cube, temp = self.G0(cube)
+        solution += temp
+        cube, temp = self.G1(cube)
+        solution += temp
+        cube, temp = self.G2(cube)
+        solution += temp
+        cube, temp = self.G3(cube)
+        solution += temp
+        return cube, solution
 
-
-cube = getRandomScramble(100)
-
+max = 0
 t = Thistlethwaite()
+for i in range(100):
+    cube = getRandomScramble(200)
+    cube, solution = t.Solve(cube)
+    if cube.getState()!=Cube().getState():
+        print("IT BROKE")
+        break
+    elif len(solution) > max:
+        max = len(solution)
 
-cube = t.G0(cube)
-
-cube = t.G1(cube)
-
-print(cube)
+print(max)
